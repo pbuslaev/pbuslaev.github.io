d22ea07626f0ea4c8dd5bd8a0b136199508d2c1f
Pavel Buslaev <pavel.buslaev@astx.com>
2025-01-08 20:05:22 +0000

    coordinate update

--- resources/scripts/annimation.js
+++ resources/scripts/annimation.js
@@ -9,10 +9,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const elapsedPlaneTime = (time % 5000) / 5000;
   
         const sunX = elapsedTime * 100; // Moves from 0% to 100% horizontally
-        const sunY = -Math.pow(elapsedTime - 0.5, 2) * 200 + 50; // Parabolic motion, peaks at 0.5 (center)
+        const sunY = 50+Math.pow(elapsedTime - 0.5, 2) * 200; // Parabolic motion, peaks at 0.5 (center)
 
         const planeX = 100 - elapsedPlaneTime * 100;
-        const planeY = 40 + 20 * elapsedPlaneTime;
+        const planeY = 60 - 20 * elapsedPlaneTime;
    
         sun.style.left = `${sunX}%`;
         sun.style.top = `${sunY}%`;
